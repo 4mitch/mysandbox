21e420ec22ad53b6cd457760527b9cebcf444861
Dima Fomin <dima.fomin@rocketmail.com>
2018-10-28 02:31:13 +0300

    2

---
+++ out/hello/core.js
@@ -0,0 +1,5 @@
+// Compiled by ClojureScript 1.9.946 {:static-fns true, :optimize-constants true, :target :nodejs}
+goog.provide('hello.core');
+goog.require('cljs.core');
+goog.require('cljs.core.constants');
+cljs.core.println.cljs$core$IFn$_invoke$arity$variadic(cljs.core.prim_seq.cljs$core$IFn$_invoke$arity$2(["Hello world!"], 0));
